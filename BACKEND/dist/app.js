813f179cb46814e34395f79f25bc92f1610e5b45
OgabekMengn1yozov <mengniyozovogabek05@gmail.com>
2023-01-22 00:57:53 +0500

    typescript

---
+++ BACKEND/dist/app.js
@@ -0,0 +1,30 @@
+import Fs from "fs";
+import Path from "path";
+import Express from "express";
+import CookieParser from "cookie-parser";
+let app = Express();
+// Middlewares
+app.use(Express.static("public"));
+app.use(CookieParser());
+// Settings 
+app.set("view engine", "ejs");
+app.set("views", Path.join(__dirname, "views"));
+const routesPath = Path.join(__dirname, "routes");
+Fs.readdir(routesPath, (err, files) => {
+    if (!err) {
+        files.forEach((file) => {
+            const routePath = Path.join(__dirname, "routes", file);
+            const Route = require(routePath);
+            if (Route.path && Route.router)
+                app.use(Route.path, Route.router);
+        });
+    }
+    app.get("*", (req, res) => {
+        res.render("404");
+    });
+});
+// app.use(BodyParser.json());
+// app.use(BodyParser.urlencoded({ extended: true }));
+// app.use(Express.json());
+// app.use(Express.urlencoded({ extended: true }));
+export default app;
--- BACKEND/server.js
+++
@@ -1,4 +0,0 @@
-const app = require("./app");
-const { PORT } = require("./config");
-
-app.listen(PORT, (_) => console.log(`🚀SERVER READY AT PORT ${PORT}`));
